68c0eaa20732c5474e08e35bec14b1bc6d3e7440
Renato Aguiar <renatooaa@outlook.com>
2018-08-25 09:20:41 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,103 @@
+const URL = "http://localhost:4500/bula"
+var form = '';
+
+
+function find(question) {
+    var data = {"question" : question};
+
+    $.ajax({
+        type: "GET",
+        url: URL + "?question=" + question,
+        //data: data,
+        success: success,
+        error: error,
+        dataType: "json"
+    });
+}
+
+$("#inputFile").change(function (){
+    var fileName = $(this).val();
+    if(fileName != null){
+        $('#btnUpload').prop("disabled", false);
+        form = new FormData();
+        form.append('fileUpload', event.target.files[0]);
+    } else {
+        $('#btnUpload').attr("disabled", "disabled");
+    }
+});
+
+$(document).ready(function() {
+	$('#txtPergunta').focus();
+});
+
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        send();
+    }
+});
+
+function error(jqXHR, exception){
+    console.log("Status: " + jqXHR.status);
+    console.log("Message: " + jqXHR.responseText);
+    if(jqXHR.responseText != undefined){
+        $("#txtResposta").text(jqXHR.responseText)
+        $('#divResposta').removeClass("d-none");
+    }
+}
+
+function success(data) {
+    console.log(data);
+    $("#txtResposta").text(data.response)
+    $('#divResposta').removeClass("d-none");
+}
+
+$('#btnEnviar').on('click', function(){
+    send();
+});
+
+$('#btnBackUpload').on('click', function(){
+    location.reload();
+});
+
+$('#btnUpload').on('click', function(){
+    var $this = $(this);
+    $('#btnUpload').attr("disabled", "disabled");
+    var loadingText = '<i class="fa fa-circle-o-notch fa-spin"></i> Upload...';
+    if ($(this).html() !== loadingText) {
+      $this.data('original-text', $(this).html());
+      $this.html(loadingText);
+    }
+    /* $.ajax({
+        url: URL + "path", // Url do lado server que vai receber o arquivo
+        data: form,
+        processData: false,
+        contentType: false,
+        type: 'POST',
+        success: function (data) {
+            // utilizar o retorno
+        }
+    }); */
+    setTimeout(function() {
+      $this.html($this.data('original-text'));
+      $('#btnUpload').attr("disabled", "disabled");
+      habilitarCampos();
+    }, 5000);
+});
+
+function send(){
+    var question = $('#txtPergunta').val();
+    if(question != '' && question != undefined){
+        find(question);
+    }
+}
+
+$('#btnLimpar').on('click', function(){	
+    $('#txtResposta' ).empty();
+    $('#divResposta').addClass("d-none");
+    $('#txtPergunta').val('');
+});
+
+function habilitarCampos() {
+    $('#upload').addClass("d-none");
+    $('#conteudo').removeClass("d-none");
+}
